1cf9881dc55d8e2ef2daf391ff65d420f5d4f578
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2024-12-08 10:43:55 +0530

    feat: search functionality

--- src/components/article/ArticlesContainer.js
+++ src/components/article/ArticlesContainer.js
@@ -1,8 +1,21 @@
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import ArticleCard from "./ArticleCard";
+import ArticleSearchBar from "./ArticleSearchBar";
 
 const ArticlesContainer = () => {
   const { articles, loading } = useSelector((state) => state.news);
+  const [search, setSearch] = useState("");
+  const [filteredArticles, setFilteredArticles] = useState(articles || []);
+
+  useEffect(() => {
+    const filteredArticles = articles.filter(
+      (article) =>
+        article?.title?.toLowerCase().includes(search.toLowerCase()) ||
+        article?.description?.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredArticles(filteredArticles);
+  }, [search]);
 
   if (loading) {
     return (
@@ -14,8 +27,9 @@ const ArticlesContainer = () => {
 
   return (
     <div className="p-4 md:p-6 lg:p-8 bg-gray-50">
+      <ArticleSearchBar search={search} setSearch={setSearch} />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6  mx-auto">
-        {articles.map((article, index) => (
+        {filteredArticles?.map((article, index) => (
           <ArticleCard key={article.url || index} article={article} />
         ))}
       </div>

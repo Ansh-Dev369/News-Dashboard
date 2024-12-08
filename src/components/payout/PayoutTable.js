b41af163936ac1c25b22a299b19588aae2872488
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2024-12-08 19:06:33 +0530

    chore

--- src/components/payout/PayoutTable.js
+++ src/components/payout/PayoutTable.js
@@ -58,8 +58,8 @@ export function PayoutTable() {
     return acc;
   }, {});
 
-  const authorRows = Object.entries(authorStats)
-    .map(([author, stats]) => ({
+  const authorRows = Object?.entries(authorStats)
+    ?.map(([author, stats]) => ({
       author,
       ...stats,
     }))
